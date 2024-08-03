6b6488959e3e1da7ca426d6071e314c2556e49f9
LuisFBM <luisfelipedemattos01@gmail.com>
2024-08-03 11:49:06 -0300

    Mudanças feitas

--- js/INDEX.js
+++ js/INDEX.js
@@ -1,3 +1,3 @@
 document.getElementById("texto").innerHTML = "Minha primeira <b>Poesia<b>!";
 console.log("Romance");
-  
+  Document.getElementById("texto").innerHTML = "<b>O Amor é algo indescritiveu para todos ninguem sabe oque é alem de se imaginar tal siginificado<b> "
